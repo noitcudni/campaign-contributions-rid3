6512a65d61675945d1e6eb2777d09451f95c9a36
Lih Chen <lihster@gmail.com>
2017-12-27 16:14:56 -0500

    fx and fy are updating when drag, but the nodes are not moving

--- src/cljs/viz/views.cljs
+++ src/cljs/viz/views.cljs
@@ -12,10 +12,10 @@
                 ;; TODO: customize link's id https://github.com/d3/d3-force#links
                 (.force "link" (.id (-> js/d3 .forceLink) (fn [d] (.-id d))))
                 (.force "charge" (js/d3.forceManyBody))
-                ;; (.force "center" (js/d3.forceCenter 500 500))
                 (.force "center" (js/d3.forceCenter (/ (:width @ratom) 2)
                                                     (/ (:height @ratom) 2)))
                 )
+        _ (re-frame/dispatch-sync [:set-var :sim sim])
         node-dataset (clj->js (-> @ratom
                                   (get :dataset)
                                   (get :nodes)))
@@ -52,66 +52,87 @@
     ))
 
 (defn force-viz [ratom]
-  [rid3/viz
-   {:id "force"
-    :ratom ratom
-    :svg {:did-mount (fn [node ratom]
-                       (.log js/console "here? " (pr-str @ratom))
-                       (-> node
-                           (.attr "width" (:width @ratom))
-                           (.attr "height" (:height @ratom))
-                           (.style "background-color" "grey")))
-          ;; :did-update (fn [node ratom]
-                        ;; (.log js/console "updating svg")
-                        ;; (-> node
-                            ;; .transition
-                            ;; (.duration transition-duration)
-                            ;; (.attr "width" (:width @ratom))
-                            ;; (.attr "height" (:height @ratom))))
-          }
+  (let [drag-started (fn [d idx]
+                       (let [sim @(re-frame/subscribe [:get-var :sim])
+                             d (-> sim .nodes (get idx))
+                             _ (.log js/console "drag-started: " ) ;;xxx
+                             ]
+                         (when-not (-> js/d3 .-event .-active)
+                           (-> sim (.alphaTarget 0.3) (.restart)))
+                         (set! (.-fx d) (.-x d))
+                         (set! (.-fy d) (.-y d))))
+        dragged (fn [_ idx]
+                  (let [sim @(re-frame/subscribe [:get-var :sim])
+                        d (-> sim .nodes (get idx))
+                        _ (.log js/console "x: " (.-x d))
+                        _ (.log js/console "y: " (.-y d))
+                        _ (.log js/console "fx: " (.-fx d))
+                        _ (.log js/console "fy: " (.-fy d))]
+                    (.log js/console "dragged: " (pr-str d)) ;;xxx
+                    (set! (.-fx d) (.-x js/d3.event))
+                    (set! (.-fy d) (.-y js/d3.event))))
+        drag-ended (fn [_ idx]
+                     (.log js/console "drag-ended: " idx) ;;xxx
+                     (let [sim @(re-frame/subscribe [:get-var :sim])
+                           d (-> sim .nodes (get idx))]
+                       (when-not (-> js/d3 .-event .-active)
+                         (-> sim (.alphaTarget 0)))
+                       (set! (.-fx d) nil)
+                       (set! (.-fy d) nil)))]
+    [rid3/viz
+     {:id "force"
+      :ratom ratom
+      :svg {:did-mount (fn [node ratom]
+                         (-> node
+                             (.attr "width" (:width @ratom))
+                             (.attr "height" (:height @ratom))
+                             (.style "background-color" "grey"))
+                         )}
+      :pieces
+      [{:kind :elem-with-data
+        :tag "circle"
+        :class "node" ;;TODO customize circle class. WARNING: class can't be an empty string
+        :did-mount (fn [node ratom]
+                     (.log js/console ">> circle")
+                     (.log js/console node) ;;xxx
+                     (let [r (-> node
+                                 (.attr "r" (fn [d]
+                                              5))
+                                 (.attr "fill" (fn [n]
+                                                 "red"))
+                                 (.call (-> js/d3
+                                            (.drag)
+                                            (.on "start" drag-started)
+                                            (.on "drag" dragged)
+                                            (.on "end" drag-ended))))]
+                       (re-frame/dispatch-sync [:set-var :node-elems r])))
+        :prepare-dataset (fn [ratom]
+                           (-> @ratom
+                               (get :dataset)
+                               (get :nodes)
+                               clj->js))}
 
-    :pieces
-    [{:kind :elem-with-data
-      :tag "circle"
-      :class "node" ;;TODO customize circle class. WARNING: class can't be an empty string
-      :did-mount (fn [node ratom]
-                   (.log js/console ">> circle")
-                   (.log js/console node) ;;xxx
+       {:kind :elem-with-data
+        :tag "line"
+        :class "link" ;;TODO customize link class
+        :did-mount (fn [node ratom]
+                     (.log js/console ">> line")
+                     (.log js/console node)
+                     (let [r (-> node
+                                 (.attr "stroke-width" 1)
+                                 (.attr "stroke" "#E5E5E5")
+                                 )]
+                       (re-frame/dispatch-sync [:set-var :link-elems r])))
+        :prepare-dataset (fn [ratom]
+                           (-> @ratom
+                               (get :dataset)
+                               (get :links)
+                               clj->js))}
 
-                   (let [r (-> node
-                               (.attr "r" (fn [d]
-                                            5
-                                            ))
-                               (.attr "fill" (fn [n]
-                                               "red")))]
-                     (re-frame/dispatch-sync [:set-var :node-elems r])))
-      :prepare-dataset (fn [ratom]
-                         (-> @ratom
-                             (get :dataset)
-                             (get :nodes)
-                             clj->js))}
-
-     {:kind :elem-with-data
-      :tag "line"
-      :class "link" ;;TODO customize link class
-      :did-mount (fn [node ratom]
-                   (.log js/console ">> line")
-                   (.log js/console node)
-                   (let [r (-> node
-                               (.attr "stroke-width" 1)
-                               (.attr "stroke" "#E5E5E5")
-                               )]
-                     (re-frame/dispatch-sync [:set-var :link-elems r])))
-      :prepare-dataset (fn [ratom]
-                         (-> @ratom
-                             (get :dataset)
-                             (get :links)
-                             clj->js))}
-
-     {:kind :raw
-      :did-mount sim-did-update
-      :did-update sim-did-update}
-     ]}])
+       {:kind :raw
+        :did-mount sim-did-update
+        :did-update sim-did-update}
+       ]}]))
 
 (defn main-panel []
   (fn []
